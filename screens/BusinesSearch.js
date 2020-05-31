c1e51718c009581fe8b3b7a1368f1b887260a1d2
Estefan Hu <estefanhu074@gmail.com>
2020-05-31 02:33:01 -0700

    eod

--- screens/BusinesSearch.js
+++ screens/BusinesSearch.js
@@ -4,11 +4,13 @@ import {
   View,
   Text,
 } from 'react-native';
+import yelp from '../api/yelp.js';
 
 import { BusinesSearchBar } from '../components/BusinesSearchBar.js';
 
 export const BusinesSearch = () => {
   const [term, setTerm] = useState('');
+  const [results, setResult] = useState([]);
 
   return (
     <View style={styles.container}>
@@ -18,6 +20,7 @@ export const BusinesSearch = () => {
         onTermSubmit={console.log}
       />
       <Text>{term}</Text>
+      <Text>{results.lenfth}</Text>
     </View>
   )
 }
