15dfd9febc94ba8bd85677669cfaca085bc70871
Estefan Hu <estefanhu074@gmail.com>
2020-05-31 02:42:40 -0700

    debugged api

--- screens/BusinesSearch.js
+++ screens/BusinesSearch.js
@@ -10,17 +10,28 @@ import { BusinesSearchBar } from '../components/BusinesSearchBar.js';
 
 export const BusinesSearch = () => {
   const [term, setTerm] = useState('');
-  const [results, setResult] = useState([]);
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('./search',{
+      params: {
+        limit: 50,
+        term,
+        location: 'Seattle'
+      }
+    });
+    setResults(response.data.businesses);
+  }
 
   return (
     <View style={styles.container}>
       <BusinesSearchBar
         term={term}
         onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={console.log}
+        onTermSubmit={searchApi}
       />
       <Text>{term}</Text>
-      <Text>{results.lenfth}</Text>
+      <Text>{results.length}</Text>
     </View>
   )
 }
