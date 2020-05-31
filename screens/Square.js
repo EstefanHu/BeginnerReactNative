c996f7af14d11dc486d159d5ea513e4ae0863730
Estefan Hu <estefanhu074@gmail.com>
2020-05-31 01:05:01 -0700

    prepping reducer

--- screens/Square.js
+++ screens/Square.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import {
   StyleSheet,
   View,
@@ -7,52 +7,38 @@ import {
 
 import { ColorCounter } from '../components/ColorCounter.js';
 
+COLOR_INCREMENT = 15;
+
+const reducer = (state, action) => {
+    
+}
+
 export const Square = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
 
-  const ADJUST_BY = 15;
-
-  // color === 'red' || 'gree' || 'blue
-  const setColor = (color, change) => {
-    switch (color) {
-      case 'red':
-        red + change > 255 || red + change < 0 ? null : setRed(red + change);
-        return;
-      case 'green':
-        green + change > 255 || green + change < 0 ? null : setGreen(green + change);
-        return;
-      case 'blue':
-        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
-        return;
-      default:
-        return;
-    }
-  }
 
   return (
     <View style={{ alignItems: 'center' }}>
       <ColorCounter
         color="Red"
         onIncrease={
-          () => setColor('red', ADJUST_BY)}
+          () => }
         onDecrease={
-          () => setColor('red', -ADJUST_BY)}
+          () => }
       />
       <ColorCounter
         color="Green"
         onIncrease={
-          () => setColor('green', ADJUST_BY)}
+          () => }
         onDecrease={
-          () => setColor('green', -ADJUST_BY)}
+          () => }
       />
       <ColorCounter
         color="Blue"
         onIncrease={
-          () => setColor('blue', ADJUST_BY)}
+          () =>}
         onDecrease={
-          () => setColor('blue', -ADJUST_BY)}
+          () =>}
       />
       <View style={{
         height: 300,
