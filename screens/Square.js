61b342ee7d041764b8ec4d6bd3b0c30873e71858
Estefan Hu <estefanhu074@gmail.com>
2020-05-31 01:15:13 -0700

    added reducers

--- screens/Square.js
+++ screens/Square.js
@@ -7,11 +7,11 @@ import {
 
 import { ColorCounter } from '../components/ColorCounter.js';
 
-COLOR_INCREMENT = 15;
+const COLOR_INCREMENT = 15;
 
+// state === { red: 0, green: 0, blue: 0 }
+// action === { colorToChange: 'red' || 'green' || 'blue'}
 const reducer = (state, action) => {
-  // state === { red: 0, green: 0, blue: 0 }
-  // action === { colorToChange: 'red' || 'green' || 'blue'}
   switch (action.colorToChange) {
     case 'red':
       return { ...state, red: state.red + action.amount };
@@ -21,36 +21,35 @@ const reducer = (state, action) => {
       return { ...state, blue: state.blue + action.amount };
     default:
       return state;
-
   }
-}
+};
 
 export const Square = () => {
   const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
-
+  const { red, green, blue } = state;
 
   return (
     <View style={{ alignItems: 'center' }}>
       <ColorCounter
         color="Red"
         onIncrease={
-          () => }
+          () => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })}
         onDecrease={
-          () => }
+          () => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })}
       />
       <ColorCounter
         color="Green"
         onIncrease={
-          () => }
+          () => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })}
         onDecrease={
-          () => }
+          () => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })}
       />
       <ColorCounter
         color="Blue"
         onIncrease={
-          () =>}
+          () => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
         onDecrease={
-          () =>}
+          () => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })}
       />
       <View style={{
         height: 300,
