8c08cf8a628d02b96232fbc6adca1f454d800e65
Estefan Hu <estefanhu074@gmail.com>
2020-05-31 00:05:15 -0700

    started random color generator

---
+++ screens/RandomColors.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+export const RandomColor = () => {
+  return (
+
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+  }
+})
