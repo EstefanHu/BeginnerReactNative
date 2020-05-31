3257e90bf702fa2308bf6f0f45a1c14d45e20725
Estefan Hu <estefanhu074@gmail.com>
2020-05-31 00:41:44 -0700

    added color changers

--- screens/Square.js
+++ screens/Square.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   StyleSheet,
   View,
@@ -8,11 +8,15 @@ import {
 import { ColorCounter } from '../components/ColorCounter.js';
 
 export const Square = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
   return (
     <View>
-      <ColorCounter color="Red"/>
-      <ColorCounter color="Green"/>
-      <ColorCounter color="Blue"/>
+      <ColorCounter color="Red" onIncrease={() => setRed(red + 1)} onDecrease={() => setRed(red - 1)} />
+      <ColorCounter color="Green" onIncrease={() => setGreen(green + 1)} onDecrease={() => setGreen(green - 1)} />
+      <ColorCounter color="Blue" onIncrease={() => setBlue(blue + 1)} onDecrease={() => setBlue(blue - 1)} />
     </View>
   )
 }
